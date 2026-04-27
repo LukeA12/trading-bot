c35639a6cc4e43baf818799cd15e0ba9b25dec07
LukeA12 <lukeauderer@yahoo.com>
2026-04-26 22:53:26 -0500

    Changing frontend target

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://54.208.220.15:8000',
         changeOrigin: true
       }
     }
